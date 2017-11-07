c23b7e4b90f9bc15c52b1461fc886f5e61cebf43
gonzalezd93 <33464682+gonzalezd93@users.noreply.github.com>
2017-11-07 14:42:41 -0500

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLib2
+{
+    public class Game
+    {
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Team1score { get; set; }
+        public int Team2score { get; set; }
+
+        public Game() { } // default constructor
+
+        public Game(string Team1, string Team2, int Team1score, int Team2score)
+        {
+            this.Team1 = Team1;
+            this.Team2 = Team2;
+            this.Team1score = Team1score;
+            this.Team2score = Team2score;
+        }
+
+        public override string ToString()
+        {
+            return Team1 + " (" + Team1score + ") " + " - " + Team2 + " (" + Team2score + ")";
+        }
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace GameLib2
+{
+    public class GameFactory
+    {
+        public List<Game> games;
+
+        public List<Game> dsgames;
+
+        public string filepath;
+
+        StreamWriter sw;
+
+        StreamReader sr;
+
+        XmlSerializer serial;
+
+        public void CreateGameList()
+        {
+            games = new List<Game>();
+            Game one = new Game("Heat", "Celtic", 89, 70);
+            games.Add(one);
+            one = new Game("Hawks", "Clippers", 75, 99);
+            games.Add(one);
+            one = new Game("Lakers", "Spurs", 90, 90);
+            games.Add(one);
+            one = new Game("Magic", "Rockets", 85, 112);
+            games.Add(one);
+            one = new Game("Pelicans", "Kings", 55, 70);
+            games.Add(one);
+            one = new Game("Thunder", "Warriors", 110, 115);
+            games.Add(one);
+
+
+        }
+
+        public Boolean SerializeGameList()
+        {
+
+            sw = new StreamWriter(filepath);
+            serial = new XmlSerializer(games.GetType());
+            serial.Serialize(sw, games);
+            sw.Close();
+            return true;
+        }
+
+        public List<Game> DesirealizeGameList()
+        {
+            dsgames = new List<Game>();
+            sr = new StreamReader(filepath);
+            serial = new XmlSerializer(dsgames.GetType());
+            dsgames = (List<Game>)serial.Deserialize(sr);
+            sr.Close();
+
+
+            return dsgames;
+
+
+           
+        }
+            
+    }
+}
